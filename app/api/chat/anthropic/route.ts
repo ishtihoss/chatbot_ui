642036a3598464fbdfc7403f834fd1e8cf4b3a49
ishtihoss <dexclaw@gmail.com>
2024-08-08 10:51:03 -0400

    checkpoint

--- app/api/chat/anthropic/route.ts
+++ app/api/chat/anthropic/route.ts
@@ -60,15 +60,22 @@ export async function POST(request: NextRequest) {
     })
 
     try {
-      const response = await anthropic.messages.create({
-        model: chatSettings.model,
-        messages: ANTHROPIC_FORMATTED_MESSAGES,
-        temperature: chatSettings.temperature,
-        system: messages[0].content,
-        max_tokens:
-          CHAT_SETTING_LIMITS[chatSettings.model].MAX_TOKEN_OUTPUT_LENGTH,
-        stream: true
-      })
+      const response = await anthropic.messages.create(
+        {
+          model: chatSettings.model,
+          messages: ANTHROPIC_FORMATTED_MESSAGES,
+          temperature: chatSettings.temperature,
+          system: messages[0].content,
+          max_tokens:
+            CHAT_SETTING_LIMITS[chatSettings.model].MAX_TOKEN_OUTPUT_LENGTH,
+          stream: true
+        },
+        {
+          headers: {
+            "anthropic-beta": "max-tokens-3-5-sonnet-2024-07-15"
+          }
+        }
+      )
 
       try {
         const stream = AnthropicStream(response)
